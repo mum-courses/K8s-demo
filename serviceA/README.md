9defae88394aba47d1254dea1ed9fc7bf779dbad
mabdelraizk3 <mabdelrazik3@gatech.edu>
2019-11-03 10:11:28 -0600

    add example service discovery

--- serviceA/README.md
+++ serviceA/README.md
@@ -1,5 +1,43 @@
 # Getting Started
 
+This is a sample spring microservice which shows:
+
+- Expose a restful layer  (HATEAOS , HAL style) on top of a repo
+- READ environment variables ( which is populated by Kubernetes)
+    - variables are coming from configmap and secrets 
+    
+# Install
+
+- First ensure you have MiniKube and kubectl running
+- In your terminal
+
+    ```console
+    foo@bar:~$ make k8s-all-create    
+    ```
+    - refere to makefile for more details
+
+- Running inside a local docker
+     ```console
+     foo@bar:~$ make run-local 
+     foo@bar:~$ curl localhost:8080/env 
+      ```
+- Retrieve service endpoint (Kubernetes cluster)
+    ```console
+    foo@bar:~$ make k8s-service-url   
+    ``` 
+- Navigate data api  (HATEAOS)
+     ```console
+     foo@bar:~$ curl <ENDPOINT HERE>/api 
+     ```
+- Show loaded variables from k8s (will show null if running locally) 
+     ```console
+     foo@bar:~$ curl <ENDPOINT HERE>/env 
+     ```
+
+### Guides used:
+
+* [https://spring.io/guides/gs/accessing-data-rest/]()
+
 ### Reference Documentation
 For further reference, please consider the following sections:
 
@@ -9,15 +47,13 @@ For further reference, please consider the following sections:
 * [Rest Repositories](https://docs.spring.io/spring-boot/docs/2.2.0.RELEASE/reference/htmlsingle/#howto-use-exposing-spring-data-repositories-rest-endpoint)
 * [Spring Boot Actuator](https://docs.spring.io/spring-boot/docs/2.2.0.RELEASE/reference/htmlsingle/#production-ready)
 
-### Guides
+
 The following guides illustrate how to use some features concretely:
 
 * [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
 * [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
 * [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
 * [Accessing JPA Data with REST](https://spring.io/guides/gs/accessing-data-rest/)
-* [Accessing Neo4j Data with REST](https://spring.io/guides/gs/accessing-neo4j-data-rest/)
-* [Accessing MongoDB Data with REST](https://spring.io/guides/gs/accessing-mongodb-data-rest/)
 * [Building a RESTful Web Service with Spring Boot Actuator](https://spring.io/guides/gs/actuator-service/)
 
 ### Additional Links
--- serviceA/src/main/java/edu/mum/ServiceA/ServiceAController.java
+++ serviceA/src/main/java/edu/mum/ServiceA/ServiceAController.java
@@ -2,9 +2,12 @@ package edu.mum.ServiceA;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -16,15 +19,15 @@ public class ServiceAController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    @Value("${KEY1}")
+    @Value("${KEY1:#{null}}")
     private String key1;
 
-    @Value("${NEXT_SERVICE}")
+    @Value("${NEXT_SERVICE:#{null}}")
     private String nextService;
 
 
     //Read from secrets of k8s
-    @Value("${SERVICE_API_KEY}")
+    @Value("${SERVICE_API_KEY:#{null}}")
     private String apiKey;
 
     @RequestMapping("/")
@@ -42,12 +45,42 @@ public class ServiceAController {
     @RequestMapping("/env")
     public String readConfigMap() {
 
-        return "read from k8s config map:" + key1 + " : " + nextService +
-                "Fetched Secrets! : "+apiKey;
+        return "Read from k8s config map:" + key1 + " : " + nextService +
+                "<====> Fetched Secrets! : "+apiKey;
     }
 
     @RequestMapping("/services")
     public List<String> services() {
         return this.discoveryClient.getServices();
+    }
+
+    @RequestMapping("/next_instances")
+    public List<ServiceInstance> nextServiceEndPoints() {
+        return this.discoveryClient.getInstances(nextService);
+    }
+    @RequestMapping("/next")
+    public String callNext() {
+
+        StringBuffer buf=new StringBuffer();
+        buf.append(this.greeting());
+        buf.append("Calling >>>>>");
+
+        final String uri = String.format("http://%s/", nextService);
+
+        String result="UNABLE TO CALL";
+
+        try{
+            RestTemplate restTemplate = new RestTemplate();
+            result = restTemplate.getForObject(uri, String.class);
+        }
+        catch (Exception e){
+           e.printStackTrace();
+        }
+        finally {
+            buf.append(result);
+            return buf.toString();
+        }
+
+
     }
 }
